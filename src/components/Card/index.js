d3a9d52df26efcc8495c2bdc676cfb03b20d3eda
PAULO HENRIQUE MARGUTTI JUNIOR <paulo.hmjunior2@senacsp.edu.br>
2024-02-15 20:22:23 -0300

    css do form iniciado

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -5,12 +5,7 @@ const Card = ({ imgsrc }) => {
   console.log(imgsrc)
   return (
     <div className='cardAreaAll'>
-      {/* <a href='#' className='cardAreaTotal'>
-        <div className='cardArea'>
-          <img className='imgBox' src={imgsrc}/>
-          <h1 className='cardTxt'>Exemplo de texto</h1>
-        </div>
-      </a> */}
+      
         <div className='cardArea'>
           <img className='imgBox' src={imgsrc} />
         </div>

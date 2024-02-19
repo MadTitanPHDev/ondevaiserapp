2fce584957a37cb75dbfba2f7805ae3d17122d35
MARISA CORREIA DE SOUZA <marisa.csouza5@senacsp.edu.br>
2024-02-19 19:29:25 -0300

    local cadastro

---
+++ src/pages/Recantodospassaros/index.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import './styles.css'
+import imagem from './recantodospassaros.jpeg'
+
+const Recantodospassaros = () => {
+  return (
+    <section className='container'>
+    <img src={imagem} className='foto'/>
+    <div>
+        <h1>Recanto dos Pássaros</h1>
+        <div>
+            
+        </div>
+
+        
+    </div>
+
+
+
+
+
+
+
+    </section>
+    
+  )
+}
+
+export default Recantodospassaros
+
+
+ 
+

84e50171a42b3a9ac7af1c106366fe07a0f9d4c7
Paulo <paulo.marguttijr@gmail.com>
2024-02-22 14:13:29 -0300

    ajustes leves no geral

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,40 +1,42 @@
 import './style.css'
 import React from 'react'
+import Layout from '../../components/Layout'
 
 const Login = () => {
     return (
         <div className='loginBody'>
+            <Layout>
 
 
-            <div className='loginBodyArea'>
-                <div className='loginInputArea'>
-                    <input className='inputLogin' required autocomplete="off" type="text" name="username" id="username" />
-                    <label className='loginLabel' for='username'>Email</label>
+                <div className='loginBodyArea'>
+                    <div className='loginInputArea'>
+                        <input className='inputLogin' required autocomplete="off" type="text" name="username" id="username" />
+                        <label className='loginLabel' for='username'>Email</label>
+                    </div>
+
+                    <div className='passArea'>
+                        <input className='inputSenha' required autocomplete="off" type="password" name="senha" id="senha" />
+                        <label className='senhaLabel' for='senha'>Senha</label>
+                    </div>
+
+                    <div class="esqSenhaArea">
+                        <label>
+
+                            <input type='checkbox'></input>
+                            Lembre de mim.
+                        </label>
+                        <label>
+                            <a href="#">Esqueceu a senha?</a>
+                        </label>
+                    </div>
+
+                    <div className='btnArea'>
+                        <button className='btnLogin'>
+                            Entrar
+                        </button>
+                    </div>
                 </div>
-
-                <div className='passArea'>
-                    <input className='inputSenha' required autocomplete="off" type="password" name="senha" id="senha" />
-                    <label className='senhaLabel' for='senha'>Senha</label>
-                </div>
-
-                <div class="esqSenhaArea">
-                    <label>
-
-                        <input type='checkbox'></input>
-                        Lembre de mim.
-                    </label>
-                    <label>
-                        <a href="#">Esqueceu a senha?</a>
-                    </label>
-                </div>
-
-                <div className='btnArea'>
-                    <button className='btnLogin'>
-                        Entrar
-                    </button>
-                </div>
-            </div>
-
+            </Layout>
 
         </div>
 

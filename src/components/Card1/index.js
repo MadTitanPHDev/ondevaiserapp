f28d7d865d95821fbd5cc81eb334f1787e8a3f2f
MadTitanPHDev <madtitanphdev@gmail.com>
2024-08-22 19:29:31 -0300

    a

--- src/components/Card1/index.js
+++ src/components/Card1/index.js
@@ -1,20 +1,18 @@
 import React from 'react'
 import './style.css'
 
+
 const Card = ({ imgsrc }) => {
   console.log(imgsrc)
   return (
-
-
-    <a href='/Categoria' class="cardH" id='cardH'> 
+    <a href='/Categoria2' class="cardH" id='cardH1'> 
         <div class="image-box">
           <img className='imgBox' src={imgsrc} />
         </div>
-        <div class="textoCard">
-          <h2>Casas de Lazer</h2>
-          <p>Quer uma festa em um ambiente urbano? Essa é a sua categoria de escolha!</p>
+        <div class="textoCard" >
+          <h2 id='h2Card'>Chacaras e sitios</h2>
+          <p id='pCard'>Quer uma festa em um ambiente urbano? Essa é a sua categoria de escolha!</p>
         </div>
-      
     </a>
   )
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -6,6 +6,10 @@ import img2Card from '../../assets/images/Firefly events 92575.jpg';
 import img3Card from '../../assets/images/Firefly places to party 78540.jpg';
 import imgBg from '../../assets/images/cmapo.jpeg';
 import Card from '../../components/Card';
+import Card1 from '../../components/Card1';
+import Card2 from '../../components/Card2';
+// import '../../pages/Home/comands'
+
 
 const Home = () => {
   return (
@@ -13,11 +17,11 @@ const Home = () => {
       <Layout>
         <div className='homeArea'>
           <Card imgsrc={img1Card} />
+          <Card1 imgsrc={img2Card} />
+          <Card2 imgsrc={img3Card} />
         </div>
 
-        {/* <div className='backgroundHome'>
-          <img src={imgBg}></img>
-        </div> */}
+        
 
       </Layout>
 
@@ -25,5 +29,6 @@ const Home = () => {
 
   )
 }
+
 
 export default Home
